bd9efa31fb25dd879c93b1cebfffc14649dac1f3
nabilaps1 <40836630+nabilaps1@users.noreply.github.com>
2018-07-09 23:27:56 -0300

    Add files via upload

---
+++ BancoDeDados.java
@@ -0,0 +1,56 @@
+package gui;
+
+//Nabila de Paula
+//Classe que cria um banco de dados para armazenar dados de pessoa e conta
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import classesDiagrama.*;
+
+public class BancoDeDados {
+
+	public static ArrayList<PessoaFisica> pessoasFisicas = new ArrayList<PessoaFisica>();;//ArrayList pessoasFisicas armazena objeto PessoaFisica
+	public static ArrayList<PessoaJuridica> pessoasJuridicas = new ArrayList<PessoaJuridica>();//ArrayList pessoasJuridicas armazena PessoaJuridica
+	public static ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();//ArrayList funcionarios armazena Funcionario
+	
+	public static ArrayList<ContaCorrente> contasCorrente = new ArrayList<ContaCorrente>();//ArrayList contasCorrente armazena objeto ContaCorrente
+	public static ArrayList<Poupanca> poupancas = new ArrayList<Poupanca>();//ArrayList poupancas armazena objeto Poupanca
+	
+	public static void verificarERegistrar(Funcionario funcionario) {//Metodo para verificar se funcionario existe e registrar
+		boolean funcionarioExiste = false;
+		for(Funcionario f : BancoDeDados.funcionarios) {
+			if(funcionario.getCPF().equals(f.getCPF())) {//Comparando os objetos 
+				JOptionPane.showMessageDialog(null, "Um funcionário com esse CPF já foi registrado");
+				funcionarioExiste = true;
+			}
+		}
+		if(!funcionarioExiste)//funcionarioExiste =  false
+			BancoDeDados.funcionarios.add(funcionario);
+	}
+	
+	public static boolean verificarPessoaFisica(String cpf) { //Metodo para verificar Pessoa Fisica
+		
+		boolean pessoaExiste = false;//criando uma nova variavel pessoaExiste do tipo boolean falso
+		for(PessoaFisica f : BancoDeDados.pessoasFisicas) {
+			if(cpf.equals(f.getCPF())) {//Comparando os objetos cpf
+				JOptionPane.showMessageDialog(null, "Você já possui uma conta!");
+				pessoaExiste = true;
+			}
+		}
+		return pessoaExiste;
+	}
+	
+	public static boolean verificarPessoaJuridica(String cnpj) {//Metodo para verificar Pessoa Juridica
+		
+		boolean pessoaExiste = false;
+		for(PessoaJuridica f : BancoDeDados.pessoasJuridicas) {
+			if(cnpj.equals(f.getCNPJ())) {//Comparando os objetos cnpj
+				JOptionPane.showMessageDialog(null, "Essa empresa já possui uma conta!");
+				pessoaExiste = true;
+			}
+		}
+		return pessoaExiste;
+	}
+}
---
+++ Poupanca.java
@@ -0,0 +1,49 @@
+package classesDiagrama;
+
+//Autor: Márcio Junior
+//Classe que cria as possibilidades para saque e deposito em conta poupança
+
+public class Poupanca extends Conta{
+    
+	
+	public Poupanca(Pessoa cliente, String senha) throws SenhaInvalidaException {
+		geraNumero++;//gera numero, smpre incrementando +1
+		setNumero(geraNumero);
+		setSaldo(0);
+		setCliente(cliente);
+		setSenha(senha);
+	}
+
+	public void deposito(double valor, String senha) throws DepositoInvalidoException, SenhaIncorretaException {//declaração do metodo
+            // qualquer método que chame deposito deve obrigatoriamente tratar as exceções DepositoInvalidoException, SenhaIncorretaException com um try
+            //um trecho de código que chame este método deve obrigatoriamente capturar as possíveis exceções que ele lance.
+		if(CriptografarSenha.criptografar(senha).equals(getSenha())){  //Comparando os objetos 
+			if(valor >0) {
+				setSaldo(getSaldo()+valor); 
+			}
+			else {
+				throw new DepositoInvalidoException();//lança uma exceção mas não exige que ela seja tratada por seus chamadores
+			}
+		}
+		else {
+			throw new SenhaIncorretaException(); //lança uma exceção mas não exige que ela seja tratada por seus chamadores
+		}
+	}
+
+	public void saque(double valor, String senha) throws SaqueInvalidoException, SenhaIncorretaException {
+		if(CriptografarSenha.criptografar(senha).equals(getSenha())) {//Comparando os objetos
+			if (valor <= getSaldo()) {
+				if(valor > 0) {
+					setSaldo(getSaldo() - valor);
+			}
+			else 
+				throw new SaqueInvalidoException("Por favor, insira um valor positivo.");//lança uma exceção mas não exige que ela seja tratada por seus chamadores
+		}
+			else 
+				throw new SaqueInvalidoException("Você estorou seu limite! O maior saque permitido para você no momento é de R$" + getSaldo());
+	}
+		else {
+			throw new SenhaIncorretaException();//lança uma exceção mas não exige que ela seja tratada por seus chamadores
+		}
+	}
+}
